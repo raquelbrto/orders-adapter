db3e4143ae8a9acca9221da53901b7a73c0faf4d
Raquel Brito <raquelbrito662@gmail.com>
2025-07-18 19:13:19 -0300

    initial commit

---
+++ src/main/java/com/devbrito/luizalabs/ordersadapter/OrdersAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.devbrito.luizalabs.ordersadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devbrito/luizalabs/ordersadapter/OrdersAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbrito.luizalabs.ordersadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
