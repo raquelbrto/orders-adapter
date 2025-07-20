ad67d002ecdf57b34015ab33752cec6730e51bfb
Raquel Brito <raquelbrito662@gmail.com>
2025-07-19 21:56:04 -0300

    feat: create order repository

---
+++ src/main/java/com/devbrito/luizalabs/ordersadapter/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.devbrito.luizalabs.ordersadapter.repository;
+
+import com.devbrito.luizalabs.ordersadapter.domain.document.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface OrderRepository extends MongoRepository<Order, Long> {
+}
