ab9623c90f3e0d31aa0e89bcfb97b8a82f693094
Raquel Brito <raquelbrito662@gmail.com>
2025-07-21 16:34:51 -0300

    feat: create exceptions

---
+++ src/main/java/com/devbrito/luizalabs/ordersadapter/exceptions/FileNotCompatibleException.java
@@ -0,0 +1,16 @@
+package com.devbrito.luizalabs.ordersadapter.exceptions;
+
+public class FileNotCompatibleException extends RuntimeException {
+    
+    public FileNotCompatibleException(String filename, String expectedType) {
+        super("File is not compatible: " + filename + ". Expected: " + expectedType);
+    }
+    
+    public FileNotCompatibleException(String message) {
+        super(message);
+    }
+    
+    public FileNotCompatibleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+} 
