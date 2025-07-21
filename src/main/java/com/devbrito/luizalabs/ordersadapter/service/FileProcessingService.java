448cbb0bb0362a4dee04a8722c7325f1321d022c
Raquel Brito <raquelbrito662@gmail.com>
2025-07-20 23:05:34 -0300

    feat: implement processFile and calculateTotal logic

--- src/main/java/com/devbrito/luizalabs/ordersadapter/service/FileProcessingService.java
+++ src/main/java/com/devbrito/luizalabs/ordersadapter/service/FileProcessingService.java
@@ -26,4 +26,31 @@ public class FileProcessingService {
         this.fileParser = fileParser;
         this.orderMapper = orderMapper;
     }
+
+    public List<Order> processFile(MultipartFile file) throws IOException {
+        List<OrderParserDTO> parsedLines = fileParser.parseFile(file);
+
+        Map<Integer, List<OrderParserDTO>> groupedByOrderId = parsedLines.stream()
+                .collect(Collectors.groupingBy(OrderParserDTO::orderId));
+
+        return groupedByOrderId.values().stream()
+                .map(lines -> {
+                    List<Product> products = lines.stream()
+                            .map(OrderParserDTO::product)
+                            .toList();
+
+                    OrderParserDTO base = lines.get(0);
+                    BigDecimal total = calculateTotal(products);
+
+                    return orderMapper.fromOrderParserDTO(base, products, total);
+                })
+                .toList();
+    }
+
+    private BigDecimal calculateTotal(List<Product> products) {
+        return products.stream()
+                .map(Product::getValue)
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .setScale(2, RoundingMode.HALF_UP);
+    }
 }
