f6a3df72a58ddf9c2b05102eeb9f91356de1926b
Raquel Brito <raquelbrito662@gmail.com>
2025-07-20 22:18:53 -0300

    feat: create GlobalExceptionHandler

---
+++ src/main/java/com/devbrito/luizalabs/ordersadapter/controller/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.devbrito.luizalabs.ordersadapter.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
