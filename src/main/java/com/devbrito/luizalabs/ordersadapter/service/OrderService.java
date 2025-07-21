7aac69bcd4a2ec1afcabc5413334803500ab067e
Raquel Brito <raquelbrito662@gmail.com>
2025-07-21 00:07:01 -0300

    feat: implements service saveAll and endpoint

--- src/main/java/com/devbrito/luizalabs/ordersadapter/service/OrderService.java
+++ src/main/java/com/devbrito/luizalabs/ordersadapter/service/OrderService.java
@@ -1,14 +1,43 @@
 package com.devbrito.luizalabs.ordersadapter.service;
 
+import com.devbrito.luizalabs.ordersadapter.domain.document.Order;
+import com.devbrito.luizalabs.ordersadapter.domain.dto.UserOrdersResponseDTO;
+import com.devbrito.luizalabs.ordersadapter.mapper.OrderMapper;
 import com.devbrito.luizalabs.ordersadapter.repository.OrderRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private final OrderMapper orderMapper;
+
+    private final FileProcessingService fileProcessingService;
+
+    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper, FileProcessingService fileProcessingService) {
+        this.orderRepository = orderRepository;
+        this.orderMapper = orderMapper;
+        this.fileProcessingService = fileProcessingService;
+    }
+
+    public List<UserOrdersResponseDTO> saveAll(MultipartFile file) throws IOException {
+        List<Order> orders = fileProcessingService.processFile(file);
+
+        orderRepository.saveAll(orders);
+
+        return orders.stream()
+                .collect(Collectors.groupingBy(Order::getUserId, LinkedHashMap::new, Collectors.toList()))
+                .entrySet().stream()
+                .map(entry -> orderMapper.toUserResponseDTO(entry.getKey(), entry.getValue()))
+                .toList();
+    }
 }
